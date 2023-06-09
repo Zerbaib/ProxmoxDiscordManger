0de08dc41d479f684cc85a9f6ebd11c70aaa97a8
Zerbaib <hugo.hennetin@proton.me>
2023-06-09 22:41:15 +0200

    add proxmox

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import discord
+import requests
 
 TOKEN = "TOKEN"
 bot = discord.Client()
+
+PROXMOX_API_URL = "https://your-proxmox-api-url/api2/json"
+PROXMOX_NODE = "your-proxmox-node"
+PROXMOX_USER = "your-proxmox-username"
+PROXMOX_PASSWORD = "your-proxmox-password"
 
 @bot.event
 async def on_ready():
