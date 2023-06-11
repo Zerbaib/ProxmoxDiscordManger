2d7137583d7d125fdf3b1611f45252cc7698f4ee
Zerbaib <hugo.hennetin@proton.me>
2023-06-11 02:00:05 +0200

    a

--- config.py
+++
@@ -1,8 +0,0 @@
-TOKEN = "YOUR_DISCORD_BOT_TOKEN"
-
-PROXMOX_API_URL = "https://your-proxmox-api-url/api2/json"
-PROXMOX_NODE = "your-proxmox-node"
-PROXMOX_USER = "your-proxmox-username"
-PROXMOX_PASSWORD = "your-proxmox-password"
-
-ID = YOUR_ID
